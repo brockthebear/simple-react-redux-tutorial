bda1b31fa3fe07d25dd440084b7d7c17c6927877
Brock Boren <brock.boren@gmail.com>
2018-10-08 11:13:47 -0500

    Connect Post component to Redux store.

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+import { connect } from 'react-redux';
 
-export default class Post extends Component {
-  state = { post: null };
 
+class Post extends Component {
   render() {
-    const post = this.state.post ? (
+    const post = this.props.post ? (
       <div className="post">
-        <h4 className="center">{ this.state.post.title }</h4>
-        <p>{ this.state.post.body }</p>
+        <h4 className="center">{ this.props.post.title }</h4>
+        <p>{ this.props.post.body }</p>
       </div>
     ) : (
       <div className="center">Loading post...</div>
@@ -21,3 +20,15 @@ export default class Post extends Component {
     );
   }
 }
+
+// ownProps is props of the component before connecting it to redux store.
+const mapStateToProps = (state, ownProps) => {
+  let id = ownProps.match.params.postId;
+
+  return {
+    post: state.posts.find(post => post.id === id)
+  }
+
+}
+
+export default connect(mapStateToProps)(Post);
