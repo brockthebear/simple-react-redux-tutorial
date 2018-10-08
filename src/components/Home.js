27584e021b41fafe639a92d63a1d9d158bed0905
Brock Boren <brock.boren@gmail.com>
2018-10-08 10:20:18 -0500

    Add react-perf-devtool package.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
+import Pokeball from '../pokeball.png';
 
-class Home extends Component {
+export default class Home extends Component {
   state = {
     posts: [],
   }
@@ -25,9 +26,10 @@ class Home extends Component {
       posts.map(post => {
         return (
           <div className="post card" key={post.id}>
+            <img src={Pokeball} alt="A pokeball." />
             <div className="card-content">
               <Link to={`/${post.id}`}>
-                <span className="card-title">{post.title}</span>
+                <span className="card-title red-text">{post.title}</span>
               </Link>
               <p>{post.body}</p>
             </div>
@@ -46,5 +48,3 @@ class Home extends Component {
     )
   }
 }
-
-export default Home;
